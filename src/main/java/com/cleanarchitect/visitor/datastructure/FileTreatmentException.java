ac2e7888d3d245db0f1035c6418776824af84752
Chae, MinSeok <mice73jp@yahoo.co.jp>
2022-01-23 16:01:56 +0900

    Modify : added visitor patttern

---
+++ src/main/java/com/cleanarchitect/visitor/datastructure/FileTreatmentException.java
@@ -0,0 +1,12 @@
+package com.cleanarchitect.visitor.datastructure;
+
+public class FileTreatmentException extends RuntimeException{
+
+    public FileTreatmentException() {
+    }
+
+    public FileTreatmentException(String message) {
+        super(message);
+    }
+    
+}
